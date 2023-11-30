5f842daee0f25d2f6f9e960dc1e448a4ff1561db
hozaifa shaikh <hozaifask463@gmail.com>
2023-11-30 21:02:57 +0530

    add multipplication

--- operator/Arithmetic.java
+++ operator/Arithmetic.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Arithmetic {
     public static void main(String args[]){
         int A=10;
-        int B=52;  
+        int B=5;  
         // System.out.println("add=" +(A+B));
-         System.out.println("Sub=" +(A-B));
+        //  System.out.println("Sub=" +(A-B));
+         System.out.println("multiply=" +(A*B));
 
     }
 }
