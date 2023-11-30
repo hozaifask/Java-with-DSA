edaf1e912b94582a71ae0a18c46ff90cf55edaf0
hozaifa shaikh <hozaifask463@gmail.com>
2023-11-30 20:31:39 +0530

    changes

--- operator/Arithmetic.java
+++ operator/Arithmetic.java
@@ -1,5 +1,10 @@
-package operator;
+import java.util.*;
+
 
 public class Arithmetic {
-    
+    public static void main(String args[]){
+        int A=10;
+        int B=52;  
+        // System.out.println("add=" +(A+B));
+    }
 }
