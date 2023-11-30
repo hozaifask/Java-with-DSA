5e22ae60095566b7064607d740ea1f9457959a59
hozaifa shaikh <hozaifask463@gmail.com>
2023-11-30 13:49:08 +0530

    update

---
+++ JavaBasic.java
@@ -0,0 +1,31 @@
+ import java.util.*;
+ 
+ public class JavaBasic {
+    public static void main (String args []){
+      //  System.out.print("hello.....\n");
+      //  System.out.print("hello.....\n");
+      //  System.out.print("hello.....\n");
+      //  System.out.println("hello.....\n");
+      //  System.out.print("hello.....\n");
+       
+//sum of a and b
+
+// int a=10;
+// int b=5;
+// int sum=a+b;
+// System.out.println(sum);
+
+
+// take input in java
+
+Scanner sc = new Scanner(System.in);
+sytring input = sc.next();
+System.out.println(input);
+
+
+
+
+
+
+    }
+}
