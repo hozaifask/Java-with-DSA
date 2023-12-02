b8a980b99cc0b3a9d955846b29e9b72942d04f43
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-02 23:50:33 +0530

    add greater and less than equal to

--- operator/realational.java
+++ operator/realational.java
@@ -3,6 +3,9 @@ public class realational {
         int a=10;
         int b=5;
         // System.out.println(a=b);
-        System.out.println(a!=b);
+        // System.out.println(a!=b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        
     } 
 }
