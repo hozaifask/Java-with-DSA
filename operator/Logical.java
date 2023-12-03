0f58ae720cf316493f7462a0b4b3d7d69d496b46
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-03 22:25:40 +0530

    learn and gate

--- operator/Logical.java
+++ operator/Logical.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 public class Logical {
     public static void main(String args[]){
+        //  OR operator
+        // System.out.println((3>1)||(1>9));
         //  AND operator
-        System.out.println((3>1)||(1>9));
+        System.out.println((3<8) && (8>6));
     }
 }
