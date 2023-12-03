b40123b599d0027e0ddaa61712bdbafc989d90d8
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-03 22:42:22 +0530

    learn assignment operator

---
+++ operator/Assignment.java
@@ -0,0 +1,16 @@
+import  java.util.*;
+
+public class Assignment {
+    public static void main (String args[]){
+        int a= 10;
+        // learn Assignment operator
+        // int b=a;
+        // a += 10;
+        // a -=10;
+        // a *=2;
+        a /=2;
+        System.out.println(a);
+        // System.out.println(b);
+    } 
+    
+}
