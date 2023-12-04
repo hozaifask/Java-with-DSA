2fdfe2b1d89200570b2854d9c0e3ca60f867d41c
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-04 15:18:06 +0530

    learn if else

---
+++ Conditional Statement/Ifelse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Ifelse {
+    public static void main(String args[]){
+    int age=9;
+        if(age>18){
+            System.out.println("you are eligible for vote");
+        }else{
+            System.out.println("you are not eligible");
+        }
+
+    }
+    
+}
