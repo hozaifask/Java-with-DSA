6e08d092f54d493109873f8f84d0bfb9b5eaeaa8
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-04 15:24:59 +0530

    learn find greater number using if else

---
+++ Conditional Statement/Greatest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class greatest {
+    public static void main(String args[]){
+        int a=900;
+        int b=100;
+        if(a>=b){
+            System.out.println("A is greater");
+        }else{
+            System.out.println("B is greater");
+        }
+
+    }
+    
+}
