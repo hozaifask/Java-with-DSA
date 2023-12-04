11aee2ac3acd8ed449cfa7b1db9f033d1f40c45a
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-04 15:46:20 +0530

    learn find Odd and even

---
+++ Conditional Statement/OddEven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class OddEven {
+    public static void main(String args[]) {
+        int a=19;
+        if(a%2==0){
+            System.out.println("A is Even");
+        }else {
+            System.out.println("A is Odd");
+        }
+        
+    }
+    
+}
