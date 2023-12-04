96a81e79b4448346f62fe9b3168a7ac2f94810cd
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-04 15:37:00 +0530

    take input from user

--- Conditional Statement/Ifelse.java
+++ Conditional Statement/Ifelse.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Ifelse {
     public static void main(String args[]){
-    int age=9;
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
         if(age>18){
             System.out.println("you are eligible for vote");
         }else{
