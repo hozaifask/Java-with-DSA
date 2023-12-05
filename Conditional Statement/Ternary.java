a0420dd4646fbc8c4b5bd97fb1ce307c16263f1a
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-06 01:01:34 +0530

    learn ternary operator

---
+++ Conditional Statement/Ternary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Ternary {
+    public static void main(String args[]){
+        int number=9;
+        String type=((number%2)==0)?"even":"Odd";
+        System.out.println(type);
+    }
+    
+}
