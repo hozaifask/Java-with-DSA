bf8b87f9618edb14c49268032e4f3fbde6e32e9f
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-06 01:08:35 +0530

    pass and Fail using Ternary operator

---
+++ Conditional Statement/PassFail.java
@@ -0,0 +1,9 @@
+public class PassFail {
+    public static void main (String args[]){
+        int marks=27;
+        String result=(marks>=33)?"Pass":"Fail";
+        System.out.println("your result is " + result);
+
+
+    }
+}
