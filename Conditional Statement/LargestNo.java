db38e16f966a5eef4774c7ff734992bf6008b89e
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-05 13:21:31 +0530

    largest  number

---
+++ Conditional Statement/LargestNo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+public class LargestNo {
+    public static void main (String args[])
+    {
+    int a=12,b=99,c=310;
+    if(a>b && a>c){
+        System.out.println("A is Greater");
+    }else if(b>c){
+        System.out.println("B is Greater");
+    }else{
+        System.out.println("C is Greater");
+    }
+    }
+} 
+    
