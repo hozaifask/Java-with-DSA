c99bf3786b32eb09af470aa08edbf16476c65369
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-07 00:08:06 +0530

    class file

---
+++ Loop/num.java
@@ -0,0 +1,13 @@
+// package Loop;
+import java .util.*;
+public class num {
+    public static void main(String args[]){
+     int counter=1;
+     while(counter<=10){
+        System.out.print(counter +" ");
+        counter++;
+     }
+     System.out.println("printed");
+    }
+    
+}
