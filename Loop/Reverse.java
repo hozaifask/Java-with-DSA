8f3cb2deb08e542dbb17c4cfa1a70eacd1f5785b
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-08 01:52:07 +0530

    learn print reverse number

---
+++ Loop/Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main(String args[]){
+        for(int i=10899;i>0;i/=10){
+            int j=i%10;
+            System.out.print(j);
+
+        }
+    }
+}
