41de276ccbd6d731661c478bb1e8157561db8a12
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-10 11:26:33 +0530

    learn

--- Loop/PrimeOrNot.java
+++ Loop/PrimeOrNot.java
@@ -1,16 +1,4 @@
-// import java.util.*;
-// public class Primeornot {
-//     public static void main(String args[])
-//     {
-//         Scanner sc=new Scanner (System.in);
-//         int n=sc.nextInt();
-//         for( int i=2;i*i<=n;i++){
-//             if(n%i==0){
-//                 return false;
-//             }
-//         }
-//     }
-// }
+
 
 
 
