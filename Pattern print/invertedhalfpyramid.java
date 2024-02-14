bba5c0ba2decd8349138a26bbac6efb6758cac89
hozaifa shaikh <hozaifask463@gmail.com>
2024-02-14 23:58:28 +0530

    update

---
+++ Pattern print/invertedhalfpyramid.java
@@ -0,0 +1,11 @@
+public class invertedhalfpyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int line=1;line<=n;line++){
+            for(int num=1;num<=(n-1+1);num++){
+                System.out.print(num);
+            }
+            System.out.println( );
+        }
+    }  
+}
