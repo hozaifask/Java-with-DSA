fb3e6d504c3c46b0fd619f1a6f6414814e2b250d
hozaifa shaikh <hozaifask463@gmail.com>
2024-02-15 23:46:16 +0530

    updated

---
+++ other/Basic.java
@@ -0,0 +1,14 @@
+package function;
+
+public class Basic {
+    public static void printHelloWorld() {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
